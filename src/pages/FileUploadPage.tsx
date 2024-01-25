8bb2964997a77bae9070efba80aa105b19e1ace3
Anastasia <nastiacu24@gmail.com>
2024-01-25 13:19:34 +0200

    fixed register

--- src/pages/FileUploadPage.tsx
+++ src/pages/FileUploadPage.tsx
@@ -1,19 +1,19 @@
 import React, { useState, ChangeEvent, FormEvent } from 'react';
-import { Viewer } from '@react-pdf-viewer/core';
-import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
+import { Viewer, Worker } from '@react-pdf-viewer/core';
+import { pageNavigationPlugin, RenderCurrentPageLabelProps } from '@react-pdf-viewer/page-navigation';
 import '@react-pdf-viewer/core/lib/styles/index.css';
 import '@react-pdf-viewer/default-layout/lib/styles/index.css';
-import { Worker } from '@react-pdf-viewer/core';
 import Button from '@mui/material/Button';
 import Container from '@mui/material/Container';
 import TextField from '@mui/material/TextField';
 import Alert from '@mui/material/Alert';
 
 const FileUploadPage: React.FC = () => {
-  const defaultLayoutPluginInstance = defaultLayoutPlugin();
-
   const [pdfFile, setPdfFile] = useState<string | null>(null);
   const [pdfFileError, setPdfFileError] = useState<string>('');
+  const pageNavigationPluginInstance = pageNavigationPlugin()
+  const { CurrentPageLabel } = pageNavigationPluginInstance;
+
 
   const [viewPdf, setViewPdf] = useState<string | null>(null);
 
@@ -72,15 +72,46 @@ const FileUploadPage: React.FC = () => {
         </Button>
       </form>
       <br />
-      <div className="pdf-container">
-        {viewPdf && (
-          <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.11.174/build/pdf.worker.min.js">
-            <div className="viewer-wrapper">
-              <Viewer fileUrl={viewPdf} plugins={[defaultLayoutPluginInstance]} />
+
+      <div className="pdf-container"
+            style={{
+                border: '1px solid rgba(0, 0, 0, 0.3)',
+                display: 'flex',
+                flexDirection: 'column',
+                // height: '100%',
+            }}
+        >
+            <div
+                style={{
+                    alignItems: 'center',
+                    backgroundColor: '#eeeeee',
+                    borderBottom: '1px solid rgba(0, 0, 0, 0.1)',
+                    display: 'flex',
+                    justifyContent: 'center',
+                    padding: '8px',
+                }}
+            >
+                <CurrentPageLabel>
+                    {(props: RenderCurrentPageLabelProps) => (
+                        <span>{`${props.currentPage + 1} of ${props.numberOfPages}`}</span>
+                    )}
+                </CurrentPageLabel>
             </div>
-          </Worker>
-        )}
+            <div
+                style={{
+                    flex: 1,
+                    overflow: 'hidden',
+                }}
+            >
+              {viewPdf && (
+                <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.11.174/build/pdf.worker.min.js">
+                <div className="viewer-wrapper">
+                <Viewer fileUrl={viewPdf} plugins={[pageNavigationPluginInstance]} />
+                </div>
+              </Worker>
+            )}
         {!viewPdf && <>No pdf file selected</>}
+            </div>
       </div>
     </Container>
   );
--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -4,11 +4,8 @@ import {
   Box,
   TextField,
   Button,
-  FormControl,
   InputAdornment,
   IconButton,
-  InputLabel,
-  OutlinedInput,
   FormControlLabel,
   FormGroup,
   Checkbox,
@@ -32,13 +29,45 @@ const Register: FC = (): ReactElement => {
     name: "",
   });
 
+  const [errorMessages, setErrorMessages] = useState<string[]>([]);
+
+  const validate = (value: string) => {
+    const errors: string[] = [];
+  
+    if (value.length < 8) {
+      errors.push('Password is too short (minimum 8 characters).');
+    }
+  
+    const lowercaseRegex = /[a-z]/;
+    const uppercaseRegex = /[A-Z]/;
+    const numbersRegex = /\d/;
+  
+    if (!lowercaseRegex.test(value)) {
+      errors.push('Password must contain at least one lowercase letter.');
+    }
+  
+    if (!uppercaseRegex.test(value)) {
+      errors.push('Password must contain at least one uppercase letter.');
+    }
+  
+    if (!numbersRegex.test(value)) {
+      errors.push('Password must contain at least one number.');
+    }
+  
+    setErrorMessages(errors);
+  };
+  
   useEffect(() => {
     if (isLoggedIn) {
-      window.location.href = '/file_upload'; 
+      window.location.href = '/file_upload';
     }
   }, [isLoggedIn]);
 
   const handleChange = (prop: keyof State, value: string | boolean): void => {
+    if (prop === "password") {
+      validate(value as string);
+    }
+
     setFormState({ ...formState, [prop]: value });
   };
 
@@ -54,165 +83,184 @@ const Register: FC = (): ReactElement => {
 
   const handleFormSubmit = async (event: React.FormEvent) => {
     event.preventDefault();
-    try {
-      const response = await fetch('http://localhost:3001/api/v1/user/signup', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          email: formState.email,
-          password: formState.password,
-          name: formState.name,
-        }),
-      });
+    if (errorMessages.length === 0) {
+      try {
+        const response = await fetch('http://localhost:3001/api/v1/user/signup', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            email: formState.email,
+            password: formState.password,
+            name: formState.name,
+          }),
+        });
 
-      if (response.ok) {
-        const data = await response.json();
-        const authToken = data.token;
-        
-        localStorage.setItem('token', authToken);
-        console.log("Registration successful!");
-        setIsLoggedIn(true);
+        if (response.ok) {
+          const data = await response.json();
+          const authToken = data.token;
 
-      } else {
-        // Handle registration failure, show an error message, etc.
+          localStorage.setItem('token', authToken);
+          console.log("Registration successful!");
+          setIsLoggedIn(true);
+
+        } else {
+          // Handle registration failure, show an error message, etc.
+        }
+      } catch (error) {
+        // Handle network or server errors.
       }
-    } catch (error) {
-      // Handle network or server errors.
     }
   };
 
   return (
-        <Container fixed maxWidth="sm" sx ={{ padding: "20px"}}>
-          <Box
-            sx={{
-              mt: "10px",
-              mx: "auto",
-              p: "40px",
-              width: 500,
-              height: 600,
-              justifyContent: "center",
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-              bgcolor: "primary.light",
-              borderRadius: "2rem",
-              boxShadow: 1,
-              color: "primary.contrastText",
-            }}
-          >
-            <Box sx={{ mx: "auto", my: "0.5rem", alignItems: "left", justifyContent: "left" }}>
-              <h1>Register</h1>
-            </Box>
+    <Container fixed maxWidth="sm" sx={{ padding: "20px" }}>
+      <Box
+        sx={{
+          mt: "10px",
+          mx: "auto",
+          p: "40px",
+          width: 500,
+          height: 600,
+          justifyContent: "center",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          bgcolor: "primary.light",
+          borderRadius: "2rem",
+          boxShadow: 1,
+          color: "primary.contrastText",
+        }}
+      >
+        <Box sx={{ mx: "auto", my: "0.5rem", alignItems: "left", justifyContent: "left" }}>
+          <h1>Register</h1>
+        </Box>
 
-            <form onSubmit={handleFormSubmit}>
-              <Grid container spacing={2} justifyContent="center" >
-                
-                <Grid item xs={12}>
-                  <TextField
-                    variant="outlined"
-                    label="Nickname"
-                    type="text"
-                    autoComplete="Nickname"
-                    fullWidth
-                    sx={{
-                        bgcolor: "primary.contrastText",
-                        borderRadius: "0.3rem",
-                    }}
-                    value={formState.name}
-                    onChange={(e) => handleChange("name", e.target.value)}
-                    InputLabelProps={{
-                      style: { color: "primary.contrastText" },
-                    }}
-                    InputProps={{
-                      style: { color: "primary.contrastText" },
-                    }}
-                  />
-                </Grid>
-                
-                <Grid item xs={12}>
-                  <TextField
-                    variant="outlined"
-                    label="Email"
-                    type="text"
-                    autoComplete="Email"
-                    autoFocus
-                    fullWidth
-                    sx={{
-                      bgcolor: "primary.contrastText",
-                      borderRadius: "0.3rem",
-                    }}
-                    value={formState.email}
-                    onChange={(e) => handleChange("email", e.target.value)}
-                    InputLabelProps={{
-                      style: { color: "primary.contrastText" },
-                    }}
-                    InputProps={{
-                      style: { color: "primary.contrastText" },
-                    }}
-                  />
-                </Grid>
+        <form onSubmit={handleFormSubmit}>
+          <Grid container spacing={2} justifyContent="center">
+            <Grid item xs={12}>
+              <TextField
+                variant="filled"
+                label="Nickname"
+                type="text"
+                autoComplete="Nickname"
+                fullWidth
+                sx={{
+                  bgcolor: "primary.contrastText",
+                  borderRadius: "0.3rem",
+                  '& input': {
+                    paddingBottom: '14px',
+                  },
+                }}
+                value={formState.name}
+                onChange={(e) => handleChange("name", e.target.value)}
+                InputLabelProps={{
+                  style: { color: "primary.contrastText" },                  
+                }}
+                InputProps={{
+                  style: { color: "primary.contrastText" },
+                }}
+              />
+            </Grid>
 
-                <Grid item xs={12}>
-                  <FormControl 
-                    variant="outlined" 
-                    fullWidth 
-                    sx={{
-                      bgcolor: "primary.contrastText",
-                      borderRadius: "0.3rem",
-                    }}
-                  >
-                    <InputLabel htmlFor="outlined-adornment-password" style={{ color: "primary.contrastText" }}>
-                      Password
-                    </InputLabel>
-                    <OutlinedInput
-                      label="Password"
-                      id="outlined-adornment-password"
-                      type={formState.showPassword ? "text" : "password"}
-                      value={formState.password}
-                      onChange={(e) => handleChange("password", e.target.value)}
-                      endAdornment={
-                        <InputAdornment position="end">
-                          <IconButton
-                            style={{ color: "primary.main" }}
-                            aria-label="toggle password visibility"
-                            onClick={handleClickShowPassword}
-                            onMouseDown={handleMouseDownPassword}
-                            edge="end"
-                          >
-                            {formState.showPassword ? <VisibilityOff /> : <Visibility />}
-                          </IconButton>
-                        </InputAdornment>
-                      }
+            <Grid item xs={12}>
+              <TextField
+                variant="filled"
+                label="Email"
+                type="text"
+                autoComplete="Email"
+                autoFocus
+                fullWidth
+                sx={{
+                  bgcolor: "primary.contrastText",
+                  borderRadius: "0.3rem",
+                  '& input': {
+                    paddingBottom: '14px',
+                  },
+                }}
+                value={formState.email}
+                onChange={(e) => handleChange("email", e.target.value)}
+                InputLabelProps={{
+                  style: { color: "primary.contrastText" },
+                  shrink: formState.email ? true : undefined,
+                }}
+                InputProps={{
+                  style: { color: "primary.contrastText" },
+                }}
+              />
+            </Grid>
+
+
+            <Grid item xs={12}>
+              <TextField
+                variant="filled"
+                label="Password"
+                type={formState.showPassword ? "text" : "password"}
+                fullWidth
+                sx={{
+                  bgcolor: "primary.contrastText",
+                  borderRadius: "0.3rem",
+                  '& input': {
+                    paddingBottom: '14px', // Adjust padding as needed
+                    verticalAlign: formState.password ? 'baseline' : 'middle',
+                  },
+                }}
+                value={formState.password}
+                onChange={(e) => handleChange("password", e.target.value)}
+                InputLabelProps={{
+                  style: { color: "primary.contrastText" },
+                }}
+                InputProps={{
+                  style: { color: "primary.contrastText" },
+                  endAdornment: (
+                    <InputAdornment position="end" sx={{ marginTop: "0.5rem" }}>
+                      <IconButton
+                        style={{ color: "primary.main" }}
+                        aria-label="toggle password visibility"
+                        onClick={handleClickShowPassword}
+                        onMouseDown={handleMouseDownPassword}
+                        edge="end"
+                      >
+                        {formState.showPassword ? <VisibilityOff /> : <Visibility />}
+                      </IconButton>
+                    </InputAdornment>
+                  ),
+                }}
+              />
+              {errorMessages.map((message, index) => (
+                <div key={index} style={{ color: "red", marginTop: "0.5rem" }}>
+                  {message}
+                </div>
+              ))}
+            </Grid>
+
+
+            <Grid item xs={12}>
+              <FormGroup>
+                <FormControlLabel
+                  control={
+                    <Checkbox
+                      color="primary"
+                      checked={formState.showPassword}
+                      onChange={() => handleChange("showPassword", !formState.showPassword)}
                     />
-                  </FormControl>
-                </Grid>
+                  }
+                  label="Show Password"
+                />
+              </FormGroup>
+            </Grid>
 
-                <Grid item xs={12}>
-                  <FormGroup>
-                    <FormControlLabel
-                      control={
-                        <Checkbox
-                          color="primary"
-                          checked={formState.showPassword}
-                          onChange={() => handleChange("showPassword", !formState.showPassword)}
-                        />
-                      }
-                      label="Show Password"
-                    />
-                  </FormGroup>
-                </Grid>
-
-                <Grid item xs={12} >            
-                    <Button type="submit" variant="contained" fullWidth>
-                      Register
-                    </Button>
-                </Grid>
-              </Grid>
-            </form>
-          </Box>
-        </Container>
-      )}
+            <Grid item xs={12} >
+              <Button type="submit" variant="contained" fullWidth>
+                Register
+              </Button>
+            </Grid>
+          </Grid>
+        </form>
+      </Box>
+    </Container>
+  );
+};
 
 export default Register;
